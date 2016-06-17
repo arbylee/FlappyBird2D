ec691ecb47741a438026e5062db0f945160ef48d
Richard Lee <rblee88@gmail.com>
2016-06-17 23:05:16 +1000

    init

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	private Vector2 force;
+	private Rigidbody2D rigidBody;
+
+	void Awake () {
+		rigidBody = GetComponent<Rigidbody2D> ();
+		force = new Vector2 (0, 300);
+	}
+	
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			rigidBody.velocity = Vector2.zero;
+			rigidBody.AddForce (force);
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+	}
+}
