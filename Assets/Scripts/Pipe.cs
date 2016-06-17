7c1a3e5bcbcfc909b6de1bcb290af76f30760eb2
Richard Lee <rblee88@gmail.com>
2016-06-17 23:32:55 +1000

    Spawn pipes

---
+++ Assets/Scripts/Pipe.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pipe : MonoBehaviour {
+	void Start () {
+		Rigidbody2D rigidBody = GetComponent<Rigidbody2D> ();
+		rigidBody.velocity = new Vector2 (-5f, 0);
+	}
+
+	void Update () {
+		if (transform.position.x < -10) {
+			Destroy (transform.parent.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/PipeSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PipeSpawner : MonoBehaviour {
+	public GameObject pipesPrefab;
+	private float timeBetweenSpawns = 1f;
+	private Vector2 spawnPosition = new Vector2(9.5f, 0);
+	void Start () {
+		StartCoroutine (DelayedSpawnPipes ());
+	}
+
+	IEnumerator DelayedSpawnPipes(){
+		while (true) {
+			yield return new WaitForSeconds (timeBetweenSpawns);
+			SpawnPipes ();
+		}
+	}
+
+	void SpawnPipes() {
+		Instantiate (pipesPrefab, spawnPosition, Quaternion.identity);
+	}
+}
